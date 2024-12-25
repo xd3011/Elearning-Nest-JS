ca2d598ac50edde29b1df53c4992c84e2b4e9814
xd3011 <xuandat3011@gmail.com>
2024-12-25 09:32:13 +0700

    feat: template send email notification schedule meeting

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('EMAIL_HOST'),
+          secure: false,
+          auth: {
+            user: configService.get<string>('EMAIL_AUTH_USER'),
+            pass: configService.get<string>('EMAIL_AUTH_PASS'),
+          },
+        },
+        preview:
+          configService.get<string>('EMAIL_PREVIEW') === 'true' ? true : false,
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [EmailService],
   exports: [EmailService],
--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,44 +1,38 @@
 import { Schedule } from '@modules/schedule/entities/schedule.entity';
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { TEmailConfig } from '@src/config/email.config';
-import { createTransport } from 'nodemailer';
-import * as Mail from 'nodemailer/lib/mailer';
 
 @Injectable()
 export class EmailService {
-  private nodemailerTransport: Mail;
-  constructor(private readonly configService: ConfigService) {
-    // Initialization code here
-    this.nodemailerTransport = createTransport({
-      service: this.configService.getOrThrow<TEmailConfig>('email').emailHost,
-      auth: {
-        user: this.configService.getOrThrow<TEmailConfig>('email')
-          .emailAuthUser,
-        pass: this.configService.getOrThrow<TEmailConfig>('email')
-          .emailAuthPass,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-      secure: false,
-    });
-  }
+  constructor(private readonly mailerService: MailerService) {}
 
   async handleSendScheduleToEmail(schedule: Schedule) {
-    // Logic to send schedule to email
     schedule.group.members.forEach(async (member) => {
-      const options: Mail.Options = {
-        from: 'Admin',
-        to: member.user.email,
-        subject: 'Bạn có lịch họp',
-        text: `Bạn có lịch họp tại nhóm: ${schedule.group.name}`,
-      };
-      await this.sendEmail(options);
-    });
-  }
+      const hours = schedule.startTime.getHours();
+      const minutes = schedule.startTime.getMinutes();
+      const seconds = schedule.startTime.getSeconds();
+      const date = schedule.startTime.getDate();
+      const month = schedule.startTime.getMonth() + 1;
+      const year = schedule.startTime.getFullYear();
 
-  async sendEmail(options: Mail.Options): Promise<void> {
-    return this.nodemailerTransport.sendMail(options);
+      const time = `${hours}:${minutes < 10 ? '0' + minutes : minutes}:${
+        seconds < 10 ? '0' + seconds : seconds
+      }`;
+      const formattedDate = `${date}/${month}/${year}`;
+
+      await this.mailerService.sendMail({
+        to: member.user.email,
+        from: 'Support Team <spteamdh@gmail.com>',
+        subject: 'Thông Báo Cuộc Họp',
+        template: 'schedule',
+        context: {
+          recipientName: `${member.user.firstName} ${member.user.lastName}`,
+          groupName: schedule.group.name,
+          date: formattedDate,
+          time: time,
+          senderName: 'Support Team <spteamdh@gmail.com>',
+        },
+      });
+    });
   }
 }
